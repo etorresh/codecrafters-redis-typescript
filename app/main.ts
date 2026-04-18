de76a5a9f52019de024872a3ef0dc39ff3bcc0d2
etorresh <me@etorresh.com>
2026-04-18 00:37:56 -0600

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,9 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment the code below to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+    connection.addListener("data", (data) => {
+        connection.write("+PONG\r\n");
+    });
 });
 
 server.listen(6379, "127.0.0.1");
